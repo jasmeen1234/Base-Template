8870ae131f7c2bf516bfbff75c9748fc29772272
jasmeen1234 <jasmeenbano12@gmail.com>
2024-04-19 21:45:31 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './Home';
 
 function App() {
     return (
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import Title from '../layouts/Title'
+import { projectOne, projectTwo, projectThree } from "../../assets/index";
+import ProjectsCard from './ProjectsCard';
+
+const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="w-full py-20 border-b-[1px] border-b-black"
+    >
+      <div className="flex justify-center items-center text-center">
+        <Title
+          title="VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK"
+          des="My Projects"
+        />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
+        <ProjectsCard
+          title="SOCIAL MEDIA CLONE"
+          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          src={projectOne}
+        />
+        <ProjectsCard
+          title="E-commerce Website"
+          des="user can add variety of product to cart and view cart in basket by clicking cart button , add and remove the product and we can login also
+          user can add wish list of the product and search all type of product"
+          src={projectTwo}
+        />
+        <ProjectsCard
+          title="Chatting App"
+          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          src={projectThree}
+        />
+        <ProjectsCard
+          title="SOCIAL MEDIA CLONE"
+          des=" Our React-based chatting app facilitates real-time communication with features like instant messaging, notifications, and message history. 
+          Utilizing React's components and state management, coupled with React Router for navigation, 
+          it ensures a dynamic and intuitive experience, accessible across devices for seamless interaction."
+          src={projectThree}
+        />
+        <ProjectsCard
+          title="E-commerce Website"
+          des=" user can add variety of product to cart and view cart in basket by clicking cart button , add and remove the product and we can login also
+          user can add wish list of the product and search all type of product"
+          src={projectOne}
+        />
+        <ProjectsCard
+          title="Chatting App"
+          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          src={projectTwo}
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects
